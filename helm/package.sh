42dae82e145e46a4c3d5cb533415725b4f78c2b4
bowenjnr <46376364+bowenjnr@users.noreply.github.com>
2021-10-11 12:39:32 +0300

    Update package.sh

--- helm/package.sh
+++ helm/package.sh
@@ -11,6 +11,6 @@ cp *.tgz index.yaml /usr/share/nginx/html/
 #rm ph-ee-engine*.tgz
 echo "deployed to local repo"
 
-scp index.yaml ph-ee-engine-1.0.0-SNAPSHOT.tgz jenkins.mifos.io:/srv/data/helm-charts/
+scp index.yaml ph-ee-engine-1.0.0-SNAPSHOT.tgz http://134.195.212.215:/srv/data/helm-charts/
 echo "deployed to remote helm repo"
 
